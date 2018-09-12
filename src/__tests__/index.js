fabda519381ca3cbdd910cc4ff4cbe345608f7b8
artalar <artalar@ya.ru>
2018-09-12 09:20:29 +0300

    improve test structure

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,71 +1,70 @@
 const { createRuntype } = require('../');
 
-describe('rtcad basic', () => {
-  const t = createRuntype({ log: false, contract: true });
+describe('rtcad', () => {
+  describe('basic', () => {
+    const t = createRuntype({ log: false, contract: true });
 
-  const sqrt = t`
+    const sqrt = t`
     The function returns the square root of a number
     @arg  ${Number} number
     @return ${Number} square root
   `(number => {
-    const result = Math.sqrt(number);
-    return result;
+      const result = Math.sqrt(number);
+      return result;
+    });
+
+    test('relative function call', () => {
+      expect(sqrt(25)).toBe(5);
+    });
+
+    test('validate arguments', () => {
+      let errorMessage = '';
+
+      try {
+        sqrt('string');
+      } catch (error) {
+        errorMessage = error.message;
+      }
+
+      expect(errorMessage).toBe('Property #1 "number" is not valid');
+    });
+
+    test('validate result', () => {
+      let errorMessage = '';
+      const MathSqrt = Math.sqrt;
+      Math.sqrt = () => 'string';
+
+      try {
+        sqrt(25);
+      } catch (error) {
+        errorMessage = error.message;
+      }
+
+      Math.sqrt = MathSqrt;
+
+      expect(errorMessage).toBe('Result "square root" is not valid');
+    });
   });
-
-  test('relative function call', () => {
-    expect(sqrt(25)).toBe(5);
-  });
-
-  test('validate arguments', () => {
-    let errorMessage = '';
-
-    try {
-      sqrt('string');
-    } catch (error) {
-      errorMessage = error.message;
-    }
-
-    expect(errorMessage).toBe('Property #1 "number" is not valid');
-  });
-
-  test('validate result', () => {
-    let errorMessage = '';
-    const MathSqrt = Math.sqrt;
-    Math.sqrt = () => 'string';
-
-    try {
-      sqrt(25);
-    } catch (error) {
-      errorMessage = error.message;
-    }
-
-    Math.sqrt = MathSqrt;
-
-    expect(errorMessage).toBe('Result "square root" is not valid');
-  });
-});
-
-describe('rtcad', () => {
-  const t = createRuntype({ log: false, contract: true });
 
   test('custom type', () => {
-    let mapIsNotExistedType = false;
+    const t = createRuntype({ log: false, contract: true });
+
+    let isMapTypeExist = true;
     try {
       t`
         @arg ${Map}
       `(() => {})(new Map());
     } catch (error) {
       expect(error.message).toBe('Types.get(...) is not a function');
-      mapIsNotExistedType = true;
+      isMapTypeExist = false;
     }
-    expect(mapIsNotExistedType).toBe(true);
+    expect(isMapTypeExist).toBe(false);
 
     let typeTesterWasCall = false;
     const MapType = t.new(type => {
       typeTesterWasCall = true;
       return type instanceof Map;
     });
-    mapIsNotExistedType = false;
 
     // isn't throw
     t`
