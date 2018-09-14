2259ff7f91d0cde611db7d8f295c39113a11014d
artalar <artalar@ya.ru>
2018-09-14 09:21:12 +0300

    draft backlog

--- src/types.js
+++ src/types.js
@@ -1,3 +1,46 @@
+/*
+  ### TODO:
+    #### type
+      - `test()`
+      - "generator" (for auto generation tests)
+    #### types
+      + undefined
+      + null
+      + Boolean
+      + Number
+        + default
+        - int, float, double
+          - positive, negative (unsigned)...
+            > https://en.wikipedia.org/wiki/C_data_types
+      + String
+        + default
+        - Char
+      + Array
+      + Object
+      + Function
+      - Literal
+      - Symbol
+      - None (null | undefined)
+      - Falsy (https://developer.mozilla.org/en-US/docs/Glossary/Falsy)
+      - Truthy (https://developer.mozilla.org/en-US/docs/Glossary/Truthy)
+      - Instance
+      - Union (array?)
+      - Map
+      - Set
+      - Class
+      - Model 
+        - shape (default?)
+        - exact
+        > array of types - is a model?
+      - Tuple
+      - Collection
+    #### Mods
+      - maybe
+      - `toString()` (for key of collections)
+      - `assign()` (merge types)
+      - `diff()` (difference from 2 (or more) types)
+*/
+
 module.exports.Types = new Map([
   [undefined, type => type === undefined],
   [null, type => type === null],
