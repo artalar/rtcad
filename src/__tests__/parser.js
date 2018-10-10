3902e7eef329635f1a5d25ae870d4dbccb21ee46
artalar <artalar@ya.ru>
2018-10-10 05:04:24 +0300

    implement MD parser

---
+++ src/__tests__/parser.js
@@ -0,0 +1,99 @@
+const { parse } = require('../parser');
+
+describe('parser', () => {
+  describe('parse', () => {
+    test('empty', () => {
+      const result = parse(``);
+
+      expect(result).toEqual({
+        argsDescription: [],
+        definition: '',
+        description: '',
+        resultDescription: 'void',
+      });
+    });
+    test('definition', () => {
+      const result = parse(`
+        # Function **definition**
+      `);
+
+      expect(result).toEqual({
+        argsDescription: [],
+        definition: '# Function **definition**',
+        description: '',
+        resultDescription: 'void',
+      });
+    });
+    test('description', () => {
+      const result = parse(`
+        # Function **definition**
+        function description
+        > comment
+      `);
+
+      expect(result).toEqual({
+        definition: '# Function **definition**',
+        description: 'function description\n> comment',
+        argsDescription: [],
+        resultDescription: 'void',
+      });
+    });
+    test('returns', () => {
+      const result = parse(`
+        # Function **definition**
+        - $$secret_key0$$ output!
+      `);
+
+      expect(result).toEqual({
+        definition: '# Function **definition**',
+        description: '',
+        argsDescription: [],
+        resultDescription: '$$secret_key0$$ output!',
+      });
+    });
+    test('1 argument', () => {
+      const result = parse(`
+        # Function **definition**
+        - $$secret_key1$$ input!
+        - $$secret_key0$$ output!
+      `);
+
+      expect(result).toEqual({
+        definition: '# Function **definition**',
+        description: '',
+        argsDescription: ['$$secret_key1$$ input!'],
+        resultDescription: '$$secret_key0$$ output!',
+      });
+    });
+    test('1 argument description', () => {
+      const result = parse(`
+        # Function **definition**
+        - $$secret_key1$$ input!
+          > input description
+        - $$secret_key0$$ output!
+      `);
+
+      expect(result).toEqual({
+        definition: '# Function **definition**',
+        description: '',
+        argsDescription: ['$$secret_key1$$ input!\n> input description'],
+        resultDescription: '$$secret_key0$$ output!',
+      });
+    });
+    test('2 arguments', () => {
+      const result = parse(`
+        # Function **definition**
+        - $$secret_key1$$ input!
+        - $$secret_key2$$ input!
+        - $$secret_key0$$ output!
+      `);
+
+      expect(result).toEqual({
+        definition: '# Function **definition**',
+        description: '',
+        argsDescription: ['$$secret_key1$$ input!', '$$secret_key2$$ input!'],
+        resultDescription: '$$secret_key0$$ output!',
+      });
+    });
+  });
+});
---
+++ src/parser.js
@@ -0,0 +1,56 @@
+const remark = require('remark');
+const styleGuide = require('remark-preset-lint-markdown-style-guide');
+
+const stringify = node => remark().stringify(node);
+
+function clearAst(string) {
+  return remark()
+    .use(styleGuide)
+    .parse(
+      // remove indents, otherwise parse will invalid
+      // FIXME: save indents inside MD
+      string.replace(/\n( )*/g, '\n'),
+    );
+}
+
+function parse(ast) {
+  const { children } = clearAst(ast);
+  let definition = '';
+  let description = '';
+  let argsDescription = [];
+  let resultDescription = 'void';
+
+  // FIXME: https://github.com/remarkjs/remark/pull/364
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i];
+    // eslint-disable-next-line
+    switch (child.type) {
+      case 'heading':
+        definition = stringify(child);
+        break;
+      case 'paragraph':
+      case 'blockquote':
+        description = (description ? `${description}\n` : '') + stringify(child);
+        break;
+      case 'list': {
+        const listItems = child.children;
+        // only returns description
+        if (listItems.length === 1) {
+          resultDescription = stringify(listItems[0]);
+          break;
+        }
+
+        argsDescription = listItems
+          .slice(0, listItems.length - 1)
+          .map(listItem => listItem.children.map(stringify).join('\n'));
+
+        resultDescription = listItems[listItems.length - 1].children.map(stringify).join('');
+        break;
+      }
+    }
+  }
+
+  return { definition, description, argsDescription, resultDescription };
+}
+
+module.exports.parse = parse;
